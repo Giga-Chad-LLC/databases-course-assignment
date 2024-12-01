5652a467084750b949215091d1ba8480c48df83c
Dmitrii Art <artuhovdmitrii@gmail.com>
2024-12-01 23:08:01 +0100

    Add 2/2 functions

--- scripts/functions.sql
+++ scripts/functions.sql
@@ -1,7 +1,7 @@
 set search_path to hse_project, public;
 
 -- 1. Возвращает просмотренные пользователем с id=checkedUserId
---    фильмы за переданный временной промежуток и язык просмотра
+--    фильмы за переданный временной промежуток и язык просмотра.
 create or replace function get_fully_watched_movies_by_user(checkedUserId int, from_time timestamp, to_time timestamp)
 returns table(userId int, email text, title text, language text) as $$
 begin
@@ -26,8 +26,29 @@ $$ language plpgsql;
 -- select * from get_fully_watched_movies_by_user(1, '2023-11-01 00:00:00', '2024-12-10 00:00:00');
 
 
--- 2. группировка по языкам числа просмотренных фильмов для пользователя
+-- 2. Возвращает для пользователя c id = checkUserId для каждого языка
+--    кол-во минут просмотра фильмов на этом языке.
+create or replace function get_minutes_watched_for_language_by_user(checkedUserId int)
+returns table(language text, minutesWatched bigint) as $$
+begin
+    return query
+    with CalculatedWatchMinutes as (
+        select u.id as userId,
+            l.language,
+            coalesce(
+                extract(epoch from (vh.viewingFinishedAt - vh.viewingStartedAt)) / 60,
+                0
+            )::int as minutesViewed
+        from ViewingHistory vh
+            full join Movies mv on mv.id = vh.movieId
+            left join Languages l on vh.languageId = l.id
+            left join Users u on u.id = vh.userId
+        where vh.userId = checkedUserId
+    )
+    select cwm.language, sum(minutesViewed) from CalculatedWatchMinutes cwm
+    group by cwm.language;
+end;
+$$ language plpgsql;
 
-
--- 3. для пользователя u для каждого языка посчитать минут просмотра на этом языке
-
+-- Usage example:
+-- select * from get_minutes_watched_for_language_by_user(1);
